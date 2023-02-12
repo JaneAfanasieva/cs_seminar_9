262b49c8790df23faefc813d7b3126c995f88732
Evgenia Afanasieva <electrojanet@gmail.com>
2023-02-13 00:09:18 +0300

    added folder task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Random rnd = new Random();
+int start = rnd.Next(1, 7);
+int end = rnd.Next(start + 1, 20);
+Console.WriteLine($"M = {start}; N = {end} -> {SumNumbers(start, end)}");
+
+int SumNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return end;
+    }
+
+    return start + SumNumbers(start + 1, end);
+}
